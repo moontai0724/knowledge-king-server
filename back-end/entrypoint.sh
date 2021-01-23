17f8bb6213428536689360b739b1ec5f10f130b4
月太げつたい <me@moontai0724.tw>
2021-01-23 20:32:27 +0800

    Update back-end image, prevent using root

--- back-end/entrypoint.sh
+++ back-end/entrypoint.sh
@@ -1,26 +1,41 @@
-#!/bin/sh
+#!/bin/bash
 
-chown root:root /app -R
-cd /app
+# Check directory owner
+if [ ! "$(stat --format '%U' .)" = "node" ]; then
+  echo "[ERROR] Please check permission of code path, it should created by user at host, and map volume into /home/node/app folder."
+  exit
+fi
 
-npm install -g npm
+# If no data, clone repo; else, update.
+if [ ! -d ./.git ]; then
+  git clone https://github.com/moontai0724/knowledge-king-back-end.git .
+else
+  git pull
+fi
 
-git pull
+# Check .env file
+if [ ! -f .env ]; then
+  echo "[ERROR] Please configure .env file."
+  exit
+fi
 
-npm install -l
-
+# Start
 case $ENVIRONMENT in
   "debug" )
+    npm install --save-dev
     npm run start:debug
   ;;
   "dev" )
+    npm install --save-dev
     npm run start:dev
   ;;
   "prod" )
+    npm install
     npm run build
     npm run start:prod
   ;;
   * )
+    npm install --save-dev
     npm run start
   ;;
 esac
